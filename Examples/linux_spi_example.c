488931586896665850d7f679e0c4a84ea255c69e
Andy <git+andy@riedlinger.me>
2026-07-22 16:35:32 -0400

    Added example for linux spi

---
+++ Examples/linux_spi_example.c
@@ -0,0 +1,50 @@
+#include "tss/com/spi.h"
+#include "tss/api/sensor.h"
+
+#include <stdio.h>
+
+#define SPI_DEVICE "/dev/spidev0.0"
+
+int main() {
+    int err;
+    struct SpiComClass ser;
+    struct TSS_Com_Class *com;
+    
+    struct TSS_Sensor sensor;
+
+    create_spi_com_class(SPI_DEVICE, 900000, &ser);
+
+    com = (struct TSS_Com_Class*) &ser;
+
+    if(tss_com_open(com)) {
+        printf("Failed to open port.\r\n");
+        return -1;
+    }
+
+    tssCreateSensor(&sensor, com);
+    err = tssInitSensor(&sensor);
+    if(err) {
+        printf("Failed to initialize sensor: %d\n", err);
+        return -1;
+    }
+
+    printf("Getting data\n");
+    //Retrieve quaternion and accelerometer data
+    float quaternion[4];
+    float accel[3];
+    sensorGetTaredOrientation(&sensor, quaternion);
+    sensorGetCorrectedAccelerometerVector(&sensor, accel);
+
+    printf("Quat: %f %f %f %f\n", quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
+    printf("Accel: %f %f %f\n", accel[0], accel[1], accel[2]);
+
+    sensorSoftwareReset(&sensor);
+
+    uint64_t time;
+    sensorGetTimestamp(&sensor, &time);
+    printf("Time: %llu\n", time);
+
+    sensorCleanup(&sensor);
+
+    return 0;
+}
